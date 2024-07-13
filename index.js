08dd2d6fd629830a0d268245e20fc39751c28c37
nilpatare <nilpatare7700@gmail.com>
2024-07-13 19:20:47 +0530

    printed friend name

---
+++ index.js
@@ -0,0 +1,12 @@
+const students = [
+    "Nil",
+    "Chaitanya",
+    "Tushar",
+    "Suraj",
+    "Harshal"
+    ]
+    
+    for(const friend of students)
+    {
+        console.log(`Hello ${friend}`)
+    }
